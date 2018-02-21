16cd1489bd5fe6d298f34bc8ed4b0c7d3e68988b
pablohonney <comedown2Florida>
2018-02-21 16:10:02 +0400

    update singly linked list

--- src/linked_list/singly_linked_list.py
+++ src/linked_list/singly_linked_list.py
@@ -1,29 +1,55 @@
 class SinglyLinkedList(object):
-    def __init__(self):
+    def __init__(self, sequence=None):
         self.root = _RootNode()
         self.head = self.root
+        self.length = 0
 
-    def _get_nth_item(self, n):
-        node = self.head.next
-        # while n and
+        if sequence:
+            for item in sequence:
+                self.append(item)
+
+    def _get_nth_item(self, index):
+        if index < 0:
+            index = len(self) + index
+
+        prev = self.root
+        node = prev.next
+
+        while index and node:
+            prev = node
+            node = node.next
+            index -= 1
+
+        return prev, node
 
     # deque behaviour
     def append(self, item):
         self.head.next = _Node(item)
         self.head = self.head.next
+        self.length += 1
 
     def pop(self, index: int = -1):
-        node = self.root.next
+        prev, head = self._get_nth_item(index)
+        if not head:
+            raise IndexError()
+
+        prev.next = head.next
+        self.head = prev
+        self.length -= 1
+        return head.value
 
     # random access
     def __setitem__(self, key, value):
-        pass
+        raise NotImplementedError
 
     def __getitem__(self, item):
-        pass
+        raise NotImplementedError
 
     def __delitem__(self, key):
-        pass
+        raise NotImplementedError
+
+    def __contains__(self, item):
+        raise NotImplementedError
 
     # iterable/iterator
     def __iter__(self):
@@ -32,6 +58,16 @@ class SinglyLinkedList(object):
             yield node.value
             node = node.next
 
+    # commons
+    def __len__(self):
+        return self.length
+
+    def __str__(self):
+        return str(list(self))
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, repr(list(self)))
+
 
 class _Node(object):
     def __init__(self, value):
@@ -39,6 +75,6 @@ class _Node(object):
         self.value = value
 
 
-class _RootNode(_Node):
+class _RootNode(_Node):  # sentinel node
     def __init__(self):
         super(_RootNode, self).__init__(None)
--- tests/linked_list/test_singly_linked_list.py
+++ tests/linked_list/test_singly_linked_list.py
@@ -1,23 +1,53 @@
 import unittest
+from random import randint
 
 from hypothesis import strategies as st, given
 
-from src.linked_list import SinglyLinkedList as SLL
+from src.linked_list import SinglyLinkedList
 
 
 class TestSinglyLinkedList(unittest.TestCase):
-    @unittest.skip('TODO Linked List')
-    def test_deque_access(self):
-        sll = SLL()
-        for i in range(5):
-            sll.append(i)
 
-        sll.pop()
+    @given(st.lists(st.integers()))
+    def test_queue_pop(self, arr):
+        sll = SinglyLinkedList(arr)
+        self.assertEqual(len(sll), len(arr))
+
+        self.assertListEqual([sll.pop(0) for _ in range(len(sll))], arr)
+        self.assertEqual(len(sll), 0)
+        with self.assertRaises(IndexError):
+            sll.pop()
+
+    @given(st.lists(st.integers()))
+    def test_stack_pop(self, arr):
+        sll = SinglyLinkedList(arr)
+        self.assertEqual(len(sll), len(arr))
+
+        self.assertListEqual([sll.pop() for _ in range(len(sll))], arr[::-1])
+        self.assertEqual(len(sll), 0)
+        with self.assertRaises(IndexError):
+            sll.pop()
+
+    @given(st.lists(st.integers()))
+    def test_random_pop(self, arr):
+        sll = SinglyLinkedList(arr)
+        self.assertListEqual(list(sll), arr)
+
+        permutation = []
+        for i in range(len(arr)):
+            permutation.append(sll.pop(randint(0, len(sll)-1)))
+
+        self.assertEqual(len(sll), 0)
+        self.assertListEqual(sorted(permutation), sorted(arr))
+
+    @given(st.lists(st.integers()))
+    def test_access(self, arr):
+        serialized = repr(SinglyLinkedList(arr))
+        sll = eval(serialized)
+
+        self.assertListEqual(list(sll), arr)
 
     @given(st.lists(st.integers()))
     def test_sequential_access(self, arr):
-        sll = SLL()
-        for i in arr:
-            sll.append(i)
-
+        sll = SinglyLinkedList(arr)
         self.assertListEqual(list(sll), arr)
