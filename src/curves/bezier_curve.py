f6db957b090b9302ccc4b12a2845dd0779d01e32
arquero <dqzakoyan@gmail.com>
2018-02-24 16:22:34 +0400

    add bezier curves, take 2

--- src/curves/bezier_curve.py
+++ src/curves/bezier_curve.py
@@ -1,8 +1,22 @@
+"""
+Hello, what a wonderful world.
+
+The one of typesets, vector images & animation.
+The one of Bezier curves.
+
+There's a whole jungle behind these tiny curves.
+This is another naive home-brewed implementation.
+"""
+
+from collections import deque
+
+
+# TAKE 1
 def linspace(start, end, steps):
-    dist = end - start
-    step = dist / steps
-    for t in range(steps + 1):
-        yield start + t * step
+    distance = end - start
+    step_distance = distance / steps
+    for step in range(steps + 1):
+        yield start + step * step_distance
 
 
 def get_line(p1, p2, steps):
@@ -12,20 +26,85 @@ def get_line(p1, p2, steps):
     )
 
 
-def bezier_curve(matrix, precision, *points):
-    """
-    p1 + p2 -> p3
-    for x1,y1 and x2,y2
-      x3 = x1 x2
-      y3 = y1 y2
-      draw x3 y3
-
-
-    p1 + p2 + p3 -> p4 + p5 -> p6
-    p1 + p2 + p3 + p4 -> p5 + p6 + p7 -> p8 + p9 -> p10
-
-    """
-    for x, y in get_line(points[0], points[1], precision):
+def dummy_bezier_curve(matrix, steps, *points):  # two dimensional only
+    for x, y in get_line(points[0], points[1], steps):
         matrix[int(y)][int(x)] = '1'
 
-    # print(list(get_line(points[0], points[1], precision)))
+
+# TAKE 2
+def get_coordinate(start, end, steps, step):
+    distance = end - start
+    step_distance = distance / steps
+    return start + step * step_distance
+
+
+def get_point(p1, p2, steps, i):
+    return get_coordinate(p1[0], p2[0], steps, i), \
+           get_coordinate(p1[1], p2[1], steps, i)
+
+
+def bezier_curve(matrix, mark, steps, *points):
+    """
+    Observation.
+        There's no need to calculate all the points for all the lines.
+        all higher level lines are abstraction.
+        We use them only for a single point per iteration.
+
+    An illustration.
+
+    3 points, 5 steps example.
+    only p6 points are printed.
+
+    all the points are put on the same line for convenience.
+    this results in a strait line, which is not very illustrative.
+    but it shows the concept.
+
+
+    innermost loop
+    ------------------------->
+
+    >> step 1                     | middle loop
+    p6                            |
+    p4----------p5                |  |  outermost loop
+    p1----------p2----------p3    |  |
+                                 \/  |
+    >> step 2                        |
+          p6                         |
+        p4----------p5               |
+    p1----------p2----------p3       |
+                                     |
+    >> step 3                        |
+                p6                   |
+          p4----------p5             |
+    p1----------p2----------p3       |
+                                     |
+    >> step 4                        |
+                        p6           |
+              p4----------p5         |
+    p1----------p2----------p3       |
+                                     |
+    >> step 5                        |
+                            p6       |
+                p4----------p5       |
+    p1----------p2----------p3      \/
+    """
+
+    in_queue = deque()
+    out_queue = deque()
+    for step in range(steps + 1):  # inclusive range
+        in_queue.extend(points)
+
+        while len(in_queue) > 1:
+            p1 = in_queue.popleft()
+            while in_queue:
+                p2 = in_queue.popleft()
+                p3 = get_point(p1, p2, steps, step)
+                out_queue.append(p3)
+
+                p1 = p2
+
+            in_queue, out_queue = out_queue, in_queue
+
+        p1 = in_queue.popleft()
+        w, h = p1
+        matrix[int(h)][int(w)] = mark
--- tests/curves/test_bezier_curve.py
+++ tests/curves/test_bezier_curve.py
@@ -4,38 +4,128 @@ from src.curves import bezier_curve
 
 
 class TestBezierCurve(unittest.TestCase):
-    w = 50  # width
+    w = 30  # width
     h = 10  # height
     empty = '0'
+    mark = '1'
+    precision = 30
 
     def setUp(self):
         self.matrix = [[self.empty] * self.w for _ in range(self.h)]
-        print()
 
-    @staticmethod
-    def draw(matrix):
-        for row in matrix:
-            print(''.join(row))
+    def to_rows(self, matrix):
+        return [''.join(row) for row in matrix]
+
+    def draw(self, matrix):
+        for row in self.to_rows(matrix):
+            print(row)
 
     def test_horizontal(self):
-        bezier_curve(self.matrix, 10, (0, 0), (self.w - 1, 0))
-        self.draw(self.matrix)
+        bezier_curve(self.matrix, self.mark, self.precision, (0, 0), (self.w - 1, 0))
+        expected = [
+            '111111111111111111111111111111',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+        ]
+        self.assertEqual(self.to_rows(self.matrix), expected)
 
     def test_vertical(self):
-        bezier_curve(self.matrix, 10, (0, 0), (0, self.h-1))
-        self.draw(self.matrix)
+        bezier_curve(self.matrix, self.mark, self.precision, (0, 0), (0, self.h - 1))
+        expected = [
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+            '100000000000000000000000000000',
+        ]
+        self.assertEqual(self.to_rows(self.matrix), expected)
 
     def test_diagonal(self):
-        bezier_curve(self.matrix, 10, (0, 0), (self.w - 1, self.h-1))
-        self.draw(self.matrix)
+        bezier_curve(self.matrix, self.mark, self.precision, (0, 0), (self.w - 1, self.h - 1))
+        expected = [
+            '111000000000000000000000000000',
+            '000111000000000000000000000000',
+            '000000111000000000000000000000',
+            '000000000111100000000000000000',
+            '000000000000011100000000000000',
+            '000000000000000011100000000000',
+            '000000000000000000011110000000',
+            '000000000000000000000001110000',
+            '000000000000000000000000001110',
+            '000000000000000000000000000001',
+        ]
+        self.assertEqual(self.to_rows(self.matrix), expected)
 
-    def test_abrupt_curve(self):
-        bezier_curve(self.matrix, 10, (0, 0), (self.w//2, self.h - 1))
-        bezier_curve(self.matrix, 10, (self.w//2, self.h - 1), (self.w-1, 0))
-        self.draw(self.matrix)
+    def test_abrupt_curve_with_two_linears(self):
+        bezier_curve(self.matrix, self.mark, self.precision, (0, 0), (self.w // 2, self.h - 1))
+        bezier_curve(self.matrix, self.mark, self.precision, (self.w // 2, self.h - 1), (self.w - 1, 0))
 
-    def test_curve(self):
-        bezier_curve(self.matrix, 10, (0, 0), (self.w//2, self.h - 1), (self.w-1, 0))
-        self.draw(self.matrix)
+        expected = [
+            '110000000000000000000000000111',
+            '001100000000000000000000001100',
+            '000110000000000000000000110000',
+            '000001100000000000000011100000',
+            '000000011000000000000110000000',
+            '000000001100000000001100000000',
+            '000000000011000000110000000000',
+            '000000000000110011000000000000',
+            '000000000000011110000000000000',
+            '000000000000000100000000000000',
+        ]
+        self.assertEqual(self.to_rows(self.matrix), expected)
 
+    def test_curve_quadratic(self):
+        bezier_curve(
+            self.matrix, self.mark, self.precision,
+            (0, 0),
+            (self.w // 2, self.h - 1),
+            (self.w - 1, 0)
+        )
+        expected = [
+            '100000000000000000000000000011',
+            '011000000000000000000000001100',
+            '000111000000000000000001110000',
+            '000000111000000000001110000000',
+            '000000000111111111110000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+        ]
+        self.assertEqual(self.to_rows(self.matrix), expected)
 
+    def test_curve_cubic(self):
+        bezier_curve(
+            self.matrix, self.mark, self.precision,
+            (0, self.h // 2),
+            (self.w // 3, 0),
+            (2 * self.w // 3, self.h - 1),
+            (self.w - 1, self.h // 2)
+        )
+        expected = [
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '001111111111000000000000000000',
+            '110000000000111100000000000000',
+            '100000000000000011111100011111',
+            '000000000000000000000011100000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+            '000000000000000000000000000000',
+
+        ]
+        self.assertEqual(self.to_rows(self.matrix), expected)
