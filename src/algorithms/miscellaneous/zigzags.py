304c6434ea20dab864b6bc8abf9bc571879df139
Tigran Zakoyan <tigran.zakoyan@betconstruct.com>
2018-07-13 15:38:48 +0400

    add zigzag runs algo

---
+++ src/algorithms/miscellaneous/zigzags.py
@@ -0,0 +1,108 @@
+class LongestRun(object):
+    """
+    Keep the longest run
+    """
+
+    def __init__(self):
+        self.max_start = 0
+        self.max_length = 0
+
+    def __call__(self, start, end):
+        length = (end + 1) - start
+        if length > self.max_length:
+            self.max_length = length
+            self.max_start = start
+
+    def get_results(self):
+        return self.max_start, self.max_start + self.max_length
+
+
+class AllRuns(object):
+    """
+    Keep all runs
+    """
+
+    def __init__(self):
+        self.runs = []
+
+    def __call__(self, start, end):
+        length = (end + 1) - start
+        self.runs.append((start, start + length))
+
+    def get_results(self):
+        return self.runs
+
+
+def zigzags(sequence, handler):
+    """
+    Feed handler with zigzag runs
+
+    :param sequence:
+    :param handler:
+    :return: handler.get_results()
+
+    ------------------------------------
+
+    FSM notes.
+
+    2x2 states:
+        in zigzag run:
+            true
+            false
+        growing:
+            true
+            false
+
+    state transitions:
+
+                                direction
+                    change                   same
+                --------------------------------------------
+        z       |
+        i in    |   keep running            end the run,
+        g       |                           call the handler
+        z       |
+        a out   |   start a new run         keep running
+        g       |
+
+    ------------------------------------
+
+    performance.
+
+    time  O(n)
+    space O(1) + space(handler)
+    """
+
+    # guard.
+    if len(sequence) < 3:
+        return handler.get_results()
+
+    cur_start = 0
+    in_a_zigzag = False
+
+    # suppose no equals in a row. only ups and downs.
+    state = sequence[0] < sequence[1]
+
+    for index in range(1, len(sequence) - 1):
+        new_state = sequence[index] < sequence[index + 1]
+        if state == new_state:
+            if in_a_zigzag:
+                in_a_zigzag = False
+                handler(cur_start, index)
+            else:
+                # continue the slope
+                pass
+        else:
+            if in_a_zigzag:
+                # continue the run
+                pass
+            else:
+                # start a new run
+                in_a_zigzag = True
+                cur_start = index - 1
+            state = new_state
+
+    if in_a_zigzag:
+        handler(cur_start, len(sequence) - 1)
+
+    return handler.get_results()
---
+++ tests/miscellaneous/test_zigzags.py
@@ -0,0 +1,128 @@
+import unittest
+
+from src.algorithms.miscellaneous.zigzags import (
+    zigzags,
+    LongestRun,
+    AllRuns,
+)
+
+REGRESSION = 1
+
+
+def regression(method):
+    return unittest.skipUnless(REGRESSION, 'regression test')(method)
+
+
+get_longest_run = lambda s: zigzags(s, LongestRun())
+get_all_runs = lambda s: zigzags(s, AllRuns())
+
+
+class TestGetLongestRun(unittest.TestCase):
+    def test_0_1_2(self):
+        self.assertEqual((0, 0), get_longest_run([]))
+        self.assertEqual((0, 0), get_longest_run([1]))
+        self.assertEqual((0, 0), get_longest_run([1, 2]))
+
+    def test_3_up(self):
+        given = [1, 2, 3]
+        expected = (0, 0)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_3_down(self):
+        given = [3, 2, 1]
+        expected = (0, 0)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_3_up_down(self):
+        given = [1, 3, 2]
+        expected = (0, 3)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_4_up_down(self):
+        given = [1, 3, 2, 4]
+        expected = (0, 4)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_7_with_2_runs(self):
+        given = [5, 2, 3, 4, 2, 5, 3]
+        expected = (2, 7)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_second_of_2(self):
+        given = [1, 4, 2, 5, 6, 7, 4, 9, 2, 5]
+        expected = (4, 10)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_first_of_2(self):
+        given = [1, 4, 2, 5, 2, 6, 7, 4, 9]
+        expected = (0, 6)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_middle_of_3(self):
+        given = [5, 4, 7, 5, 1, 4, 2, 6, 1, 5, 2, 6, 7, 4, 9]
+        expected = (3, 12)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+    def test_exercise(self):
+        given = [1, 3, 2, 8, 5, 7, 6, 4, 3]
+        expected = (0, 7)
+
+        got = get_longest_run(given)
+        self.assertEqual(expected, got)
+
+
+class TestGetAllRuns(unittest.TestCase):
+    def test_4_up_down(self):
+        given = [1, 3, 2, 4]
+        expected = [(0, 4)]
+
+        got = get_all_runs(given)
+        self.assertEqual(expected, got)
+
+    def test_7_with_2_runs(self):
+        given = [5, 2, 3, 4, 2, 5, 3]
+        expected = [(0, 3), (2, 7)]
+
+        got = get_all_runs(given)
+        self.assertEqual(expected, got)
+
+    def test_second_of_2(self):
+        given = [1, 4, 2, 5, 6, 7, 4, 9, 2, 5]
+        expected = [(0, 4), (4, 10)]
+
+        got = get_all_runs(given)
+        self.assertEqual(expected, got)
+
+    def test_first_of_2(self):
+        given = [1, 4, 2, 5, 2, 6, 7, 4, 9]
+        expected = [(0, 6), (5, 9)]
+
+        got = get_all_runs(given)
+        self.assertEqual(expected, got)
+
+    def test_middle_of_3(self):
+        given = [5, 4, 7, 5, 1, 4, 2, 6, 1, 5, 2, 6, 7, 4, 9]
+        expected = [(0, 4), (3, 12), (11, 15)]
+
+        got = get_all_runs(given)
+        self.assertEqual(expected, got)
+
+
+if __name__ == '__main__':
+    unittest.main()
